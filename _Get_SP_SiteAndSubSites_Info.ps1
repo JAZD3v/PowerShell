30813e21338303b5db59eec5b552e8fad0b1691e
Jason Brown <jasonubrown@gmail.com>
2025-11-19 09:30:49 -0500

    Initial upload of script
    This script takes a top level site of a SharePoint site collection and uses PnP PowerShell to get particular info on it and its subsites and then adds that info to a SharePoint list. The properties it gathers: Title, Url, Author (site creator), LastItemUserModifiedDate.

---
+++ _Get_SP_SiteAndSubSites_Info.ps1
@@ -0,0 +1,38 @@
+﻿$Root = "Root-Site"
+$ListName = "YourListName"
+$Context = "https://YourDomain/sites/$($Root)"
+Connect-PnPOnline -Url $Context -UseWebLogin
+
+$HubSite = Get-PnPWeb -Includes SiteUsers,Author,LastItemModifiedDate,LastItemUserModifiedDate
+
+
+Clear-Variable SCAs
+$SCAs = @()
+
+foreach($User in $HubSite.SiteUsers)
+{
+
+    if($User.IsSiteAdmin -eq $True)
+    {
+    
+        $SCAs += $($User.ID)
+
+    }
+}
+
+$SubSites = Get-PnPSubWeb -Recurse <#-IncludeRootWeb#> -Includes Author,LastItemModifiedDate,LastItemUserModifiedDate 
+
+Connect-PnPOnline -Url "https://YourDomain/sites/YourSite" -UseWebLogin
+
+Add-PnPListItem -List "YourListName" `
+-Values @{"Title"=$HubSite.Title ; "ModernSiteURL"=$HubSite.Url ; "SiteCollectionAdmins"=$SCAs ; "SiteCreater" = $HubSite.SiteCreater ; "LastUserDateModified" = (get-date $HubSite.LastItemUserModifiedDate -f "MM/dd/yyyy") <#; "SubSites" = $_.SubSites#>} | Out-null
+
+
+for($i=0; $i-le ($SubSites.count - 1); $i++){
+
+   
+        Add-PnPListItem -List "$ListName" `
+        -Values @{"Title"=$($SubSites[$i].Title) ; "ModernSiteURL"=$($SubSites[$i].Url) ; "SiteCreater" = $($SubSites[$i].SiteCreater) ; "LastUserDateModified" = (get-date $SubSites[$i].LastItemUserModifiedDate -f "MM/dd/yyyy") <#; "SubSites" = $_.SubSites#>} | Out-null
+    
+
+}
