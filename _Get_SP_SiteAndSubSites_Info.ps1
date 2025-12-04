730d1940c5d189f0258a00b3356a7840ab8f1184
Jason Brown <jasonubrown@gmail.com>
2025-12-04 14:57:37 -0500

    Refactor PowerShell script for site and subsites info

--- _Get_SP_SiteAndSubSites_Info.ps1
+++ _Get_SP_SiteAndSubSites_Info.ps1
@@ -1,38 +1,49 @@
-﻿$Root = "Root-Site"
-$ListName = "YourListName"
-$Context = "https://YourDomain/sites/$($Root)"
+#Replace with your site url
+$Context = "https://YourDomain/sites/SiteName"
+
 Connect-PnPOnline -Url $Context -UseWebLogin
+
+if($Subsites.count -ge 1)
+{
+Clear-Variable SubSites
+}
+
+if($HubSite.count -eq 1)
+{
+Clear-Variable HubSite
+}
 
 $HubSite = Get-PnPWeb -Includes SiteUsers,Author,LastItemModifiedDate,LastItemUserModifiedDate
 
-
-Clear-Variable SCAs
-$SCAs = @()
-
+$SCAsInfo = @()
 foreach($User in $HubSite.SiteUsers)
 {
 
     if($User.IsSiteAdmin -eq $True)
     {
-    
-        $SCAs += $($User.ID)
-
+        $SCAsInfo += $($User)
     }
 }
 
-$SubSites = Get-PnPSubWeb -Recurse <#-IncludeRootWeb#> -Includes Author,LastItemModifiedDate,LastItemUserModifiedDate 
+$SubSites = Get-PnPSubWeb -Recurse -Includes Author,LastItemModifiedDate,LastItemUserModifiedDate 
 
-Connect-PnPOnline -Url "https://YourDomain/sites/YourSite" -UseWebLogin
+#Rplace Url with list you want to add the info to
+$ListSite= "https://YourDomain/sites/ListName"
+#Rplace with List Name
+$ListName="ListName"
 
-Add-PnPListItem -List "YourListName" `
--Values @{"Title"=$HubSite.Title ; "ModernSiteURL"=$HubSite.Url ; "SiteCollectionAdmins"=$SCAs ; "SiteCreater" = $HubSite.SiteCreater ; "LastUserDateModified" = (get-date $HubSite.LastItemUserModifiedDate -f "MM/dd/yyyy") <#; "SubSites" = $_.SubSites#>} | Out-null
+Connect-PnPOnline -Url $ListSite -UseWebLogin
 
+Add-PnPListItem -List $ListName -ContentType "Item" -Values @{"Title"=$HubSite.Title ; "ModernSiteURL"=$HubSite.Url ; "SiteCollectionAdmins"=$SCAsInfo.LoginName ; "SiteCreater" = $HubSite.Author.Email ; "LastUserDateModified" = (get-date $HubSite.LastItemUserModifiedDate -f "MM/dd/yyyy")} | Out-null
 
-for($i=0; $i-le ($SubSites.count - 1); $i++){
-
-   
-        Add-PnPListItem -List "$ListName" `
-        -Values @{"Title"=$($SubSites[$i].Title) ; "ModernSiteURL"=$($SubSites[$i].Url) ; "SiteCreater" = $($SubSites[$i].SiteCreater) ; "LastUserDateModified" = (get-date $SubSites[$i].LastItemUserModifiedDate -f "MM/dd/yyyy") <#; "SubSites" = $_.SubSites#>} | Out-null
-    
-
+if($SubSites.count -ge 1){
+    Write-Host "There are $($SubSites.count) Subsites in $($Context)" -BackgroundColor DarkGreen
+    for($i=0; $i-le ($SubSites.count - 1); $i++)
+    {
+        Add-PnPListItem -List $ListName -ContentType "Item" -Values @{"Title"=$($SubSites[$i].Title) ; "ModernSiteURL"=$($SubSites[$i].Url) ; "SiteCreater" = $($SubSites[$i].Author.Email) ; "LastUserDateModified" = (get-date $SubSites[$i].LastItemUserModifiedDate -f "MM/dd/yyyy")} | Out-null  
+    }
+}
+else
+{
+    Write-Host "There are $($SubSites.count) Subsites in $($Context)" -BackgroundColor DarkYellow
 }
